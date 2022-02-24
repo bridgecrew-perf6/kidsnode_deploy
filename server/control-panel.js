55a608e76429691492e2c4abd35c9426901bb4ad
koja5 <kojaaa95@gmail.com>
2022-02-24 12:54:14 +0100

    first commit

---
+++ server/control-panel.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+const auth = require("./config/auth");
+const logger = require("./config/logger");
+
+var connection = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+});
+
+connection.getConnection(function (err, conn) {
+  console.log(err);
+  console.log(conn);
+});
+
+/* GET api listing. */
+router.get("/", (req, res) => {
+  res.send("api works");
+});
+
+router.get("/getChildrensStatistic", auth, async (req, res, next) => {
+  try {
+    connection.getConnection(function (err, conn) {
+      if (err) {
+        logger.log("error", err.sql + ". " + err.sqlMessage);
+        res.json(err);
+      } else {
+        conn.query(
+          "select count(*) as 'number', date from record_absense where kindergarden_id = ? group by date order by date asc",
+          [req.user.user.kindergarden],
+          function (err, rows, fields) {
+            conn.release();
+            if (err) {
+              res.json(err);
+              logger.log("error", err.sql + ". " + err.sqlMessage);
+            } else {
+              res.json(rows);
+            }
+          }
+        );
+      }
+    });
+  } catch (ex) {}
+});
+
+module.exports = router;
